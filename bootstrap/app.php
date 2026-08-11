3d6249b399af4f6a4727cac7f478fea90d223b5c
shakeel <42284575+Shakeeliqbal2551@users.noreply.github.com>
2026-08-11 17:55:18 +0500

    Revert "Merge origin/main into main, resolving conflicts in favor of local work"
    This reverts commit a0d12aaa1d56d18fefb3eefb4745fb4fbad8ed68, reversing
    changes made to 3ccef9fb5948d9b29dd0c7b80753ad39ab6b5728.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,10 +7,7 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: [
-            __DIR__.'/../routes/web.php',
-            __DIR__.'/../routes/admin.php',
-        ],
+        web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
@@ -22,10 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->web(prepend: [
             EnforceCanonicalHost::class,
-        ]);
-
-        $middleware->alias([
-            'admin' => \App\Http\Middleware\EnsureAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
